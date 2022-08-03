4914b53f5868d99c1fa3ef28e8c27413c6a58703
@tbutterc <tbutterc@student.21-school.ru>
2022-08-04 02:38:20 +0700

    lin_commit_cross_1

--- src/fun.py
+++ src/fun.py
@@ -1,31 +1,20 @@
 import random
-import math as mt
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 # нарисовать маршрут
 def route_image(list_cities, route):
-    # пронумеруем города
     for i in range(list_cities.shape[0]):
         plt.annotate(i, (list_cities[i][0], list_cities[i][1]), fontsize=14)
-    # нарисуем города
     plt.scatter(list_cities[:, 0], list_cities[:, 1], c='red')
-    # нарисуем маршрут
     x = [list_cities[route[-1]][0]]
     y = [list_cities[route[-1]][1]]
     for i in range(len(route)):
         x.append(list_cities[route[i]][0])
         y.append(list_cities[route[i]][1])
     plt.plot(x, y, c='blue')
-    # выведем изображение
     plt.show()
-
-
-# if (xy == 0):
-#     print("x1: |", list_cities[route[j]][xy], "| x2: |", list_cities[route[j + 1]][xy], "|")
-# else:
-#     print("y1: |", list_cities[route[j]][xy], "| y2: |", list_cities[route[j + 1]][xy], "|\n")
 
 
 def route_length(list_cities, route):
@@ -48,7 +37,6 @@ def route_length(list_cities, route):
     return length + length_penalty
 
 
-# качество особей (маршрутов)
 def quality(list_cities, routes):
     # проведем оценку маршрутов
     length_penalty = np.zeros([len(routes)])
@@ -67,14 +55,8 @@ def generate_routes(number_cities, number_routes):
     return routes, list_cities
 
 
-# мутация маршрута
 def mutation(list_cities, routes, count_permit, count_routes, detla, max_iteration):
-    len_qu = quality(list_cities, routes)
-    short = len_qu.min()
-    if count_routes != len(routes):
-        new_routes, arr_len = selection_routes(list_cities, routes, count_routes)
-    else:
-        new_routes, arr_len =  sort_routes(list_cities, routes)
+    new_routes, arr_len = selection_routes(list_cities, routes, count_routes)
 
     new_arr_len = np.zeros(len(arr_len))
 
@@ -86,14 +68,12 @@ def mutation(list_cities, routes, count_permit, count_routes, detla, max_iterati
             new_route = permit_routes(routes[i], count_permit)
             new_arr_len[i] = route_length(list_cities, new_route)
             count += 1
-
-        # if count >= max_iteration:
-        #     print("number of iterations exceeded")
         routes[i] = new_route
 
     routes, sort_arr = selection_routes(list_cities, routes, count_routes)
 
     return routes, sort_arr
+
 
 def permit_routes(route, count_permit):
     for i in range(count_permit):
@@ -155,7 +135,71 @@ def sort_routes(list_cities, routes):
 
 def selection_routes(list_cities, routes, count_routes):
     sorted_dict, arr_len = sort_routes(list_cities, routes)
-    new_routes = np.zeros((count_routes, len(list_cities)), dtype= int)
+    new_routes = np.zeros((count_routes, len(list_cities)), dtype=int)
     for i in range(count_routes):
         new_routes[i] = sorted_dict.get(arr_len[i])
     return new_routes, arr_len
+
+
+def crossbreeding(list_cities, routes, count_routes, count_insert, delta, max_iteration):
+    new_routes, arr_len = selection_routes(list_cities, routes, count_routes)
+    cros_route = []
+    for i in range(len(new_routes) - 1):
+        for j in range(len(new_routes) - 1):
+            count = 0
+            new_route = insert_route(new_routes[i], new_routes[j], count_insert)
+            new_len = route_length(list_cities, new_route)
+            while arr_len[i] - delta < new_len and count < max_iteration:
+                new_route = insert_route(new_routes[i], new_routes[j], count_insert)
+                new_len = route_length(list_cities, new_route)
+                count += 1
+            cros_route.append(new_route)
+    cros_route = np.array(cros_route)
+    cros_route, cros_len = selection_routes(list_cities, cros_route, len(cros_route))
+
+    return cros_route, cros_len
+
+
+def insert_route(route_1, route_2, count_insert):
+    start_index = random.randint(0, len(route_1) - count_insert - 1)
+    arr_insert = np.zeros(count_insert, dtype=int)
+    route_3 = np.zeros(len(route_1), dtype=int)
+    for i in range(count_insert):
+        arr_insert[i] = route_1[i + start_index]
+    for j in range(len(route_1) - count_insert):
+        arr_part = np.zeros(count_insert, dtype=int)
+        insert_index = j
+        for k in range(count_insert):
+            arr_part[k] = route_2[k + j]
+        intersection = np.array(list(set(arr_insert) & set(arr_part)))
+        if len(intersection) == count_insert and (compare(intersection, arr_insert) == False):
+            route_3 = insert_arr(route_2, arr_insert, insert_index, count_insert)
+
+    route_4 = np.zeros(len(route_1), dtype=int)
+    if (route_3.all() == route_4.all()):
+        route_3 = route_2
+    return route_3
+
+
+def compare(arr_1, arr_2):
+    flag = False
+    range_ = len(arr_1) if len(arr_1) < len(arr_2) else len(arr_2)
+    count = 0
+    for i in range(range_):
+        if arr_1[i] == arr_2[i]:
+            count += 1
+    if count == range_:
+        flag = True
+
+    return flag
+
+
+def insert_arr(route, arr_insert, index, count_insert):
+    new_route = np.zeros(len(route), dtype=int)
+    for j in range(len(route)):
+        new_route[j] = route[j]
+
+    for i in range(index, count_insert + index):
+        new_route[i] = arr_insert[i - index]
+
+    return new_route
--- src/main.py
+++ src/main.py
@@ -2,63 +2,35 @@ import numpy as np
 import fun
 
 
-def test_length_route():
-    list_cities = np.array([[0.22545213, 0.47175503],
-                            [0.7215346, 0.01659324],
-                            [0.29019241, 0.63847256],
-                            [0.42587585, 0.11428757]])
-    print(list_cities)
-    route = np.array(range(4))
-    len = fun.route_length(list_cities, route)
-    assert (len == 2.3813634047264274)
-
-
-def base():
-    number_cities = 15  # количество городов
-    list_cities = np.random.random([number_cities, 2]) # координаты городов
-    # route = np.random.randint(number_cities, size=number_cities)  # сгенерированный маршрут
-    # print("Маршрут:", route)
-    # fun.route_image(list_cities, route)
-    # len = fun.route_length(list_cities, route)
-    routes = []
-    number_routes = 100
-    for i in range(number_routes):
-        routes.append(np.random.permutation(number_cities))
-    routes = np.array(routes)
-
-    len_qu = fun.quality(list_cities, routes)
-    min = len_qu.min()
-    print(min)
-    # index = np.where(len_qu == min)
-    index = list(len_qu).index(min)
-    print("len_q\n", len_qu)
-    print("min = ", min, " i = ", index, "l_q = ", len_qu[index])
-
-    print("Маршрут:", routes[index])
-    fun.route_image(list_cities, routes[index])
-
-def base_2():
+def main():
     number_cities, number_routes = 15, 100
     routes, list_cities = fun.generate_routes(number_cities, number_routes)
     count_permit = 1
     count_routes = int(number_routes / 4)
-    delta = 0.5
+    delta = 0.2
     max_iter = 100
+    count_insert = 5
 
     len_qu = fun.quality(list_cities, routes)
     min = len_qu.min()
     index = list(len_qu).index(min)
-    print("Route to mutation", routes[index], " len :", min)
+    print("Route to mutation:   ", routes[index], " len :", min)
     fun.route_image(list_cities, routes[index])
 
     mut_routes, arr_len = fun.mutation(list_cities, routes, count_permit, count_routes, delta, max_iter)
 
-    # print(count_routes, mut_routes, arr_len)
     for i in range(1):
-        print("Route after mutation:", mut_routes[i], " len :", arr_len[i])
+        print("Route to crossbreeding:", mut_routes[i], " len :", arr_len[i])
         fun.route_image(list_cities, mut_routes[i])
 
+    cros_route, cros_len = fun.crossbreeding(list_cities, mut_routes, count_routes, count_insert, delta, max_iter)
+
+    for i in range(1):
+        print("Route after crossbreeding:", cros_route[i], " len :", cros_len[i])
+        fun.route_image(list_cities, cros_route[i])
+
+    print("Ok")
 
 
 if __name__ == '__main__':
-    base_2()
+    main()
